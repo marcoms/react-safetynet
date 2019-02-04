1531e080e05778fc29fa091a989f9037d0998dec
Marco Scannadinari <m@scannadinari.co.uk>
2019-02-04 17:06:02 +0000

    Add error boundary logic

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,58 @@
 import * as React from 'react';
 import './index.css';
 
-type SafetyNetState = {
+interface SafetyNetState {
 	hasError: boolean;
+	error?: Error,
+	errorInfo?: React.ErrorInfo,
 }
 
-class SafetyNet extends React.Component {
+interface SafetyNetProps {
+	errorHandler?: (error: Error, errorInfo: React.ErrorInfo) => void;
+};
+
+class SafetyNet extends React.Component<SafetyNetProps, SafetyNetState> {
 	state: SafetyNetState = {
 		hasError: false,
 	};
 
-	constructor(props: any) {
+	constructor(props: SafetyNetProps) {
 		super(props);
 	}
 
+	componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+		this.setState({
+			hasError: true,
+			error,
+			errorInfo,
+		});
+
+		if (typeof this.props.errorHandler === 'function') {
+			return this.props.errorHandler(error, errorInfo);
+		}
+
+		console.error('[SafetyNet] Recived error:', error, errorInfo);
+	}
+
 	render() {
-		return (
-			<div className="SafetyNet">
-				{this.props.children}
-			</div>
-		)
+		if (!this.state.hasError) {
+			return (
+				<div className="SafetyNet">
+					{this.props.children}
+				</div>
+			);
+		} else {
+			return (
+				<div className="SafetyNet SafetyNet--error">
+					<p className="SafetyNet__ErrorHeader">Error encountered within this boundary:</p>
+
+					<pre className="SafetyNet__ErrorDetails">
+						{this.state.error.name}: {this.state.error.message}
+						{this.state.errorInfo.componentStack}
+					</pre>
+				</div>
+			)
+		}
 	}
 }
 
